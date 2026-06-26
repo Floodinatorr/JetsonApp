255c8f1602861144edd450de058e9602ee296ad4
floodinatorr <emirunalan44@gmail.com>
2026-06-26 14:37:19 +0300

    OpenCV kurulumu yapıldı, test edildi.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <opencv4/opencv2/opencv.hpp>
 
 int main(){
+    std::string imagePath = "test.jpg";
+    cv::Mat image = cv::imread(imagePath, cv::IMREAD_COLOR);
+    cv::imshow("Jetson App", image);
+    cv::waitKey(0);
+
     std::cout<<"Hello World!"<<std::endl;
+    std::cout<<"Hello World2"<<std::endl;
     return 0;
 }
