e80f93a07c17d1793a2c48e6abba89a28581bd8b
floodinatorr <emirunalan44@gmail.com>
2026-06-26 15:45:45 +0300

    Hazır YOLO modellerinin kurulumu ve ONNX formatına çevirilimesi için komutlar ayarlandı.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # JetsonApp
+
+Python 3.11.15
+CUDA 13.3 && TensorRT 11.1.0.6
---
+++ models/onnx2trt.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python -m venv model_env
+source model_env/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+yolo export model=yolo11s.pt format=onnx device=0
+deactivate
